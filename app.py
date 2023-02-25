7e0e0c3f61a63ee2502797b71cd8bf2db2fa0cf3
tehila17-meet <tehilapelled16@gmail.com>
2023-02-25 13:29:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 cors = CORS(app)
 main_arango_handler = ArangoHandler()
 print(os.getcwd(), "current")
-arango_exe_path = "arango_server\\usr\\bin\\arangod.exe"
+arango_exe_path = "./arango_server/usr/bin/arangod.exe"
 arango_process = subprocess.Popen([arango_exe_path])
 
 
